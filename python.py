30ec808cf9a786784df3948176e08fee2567750d
CC0369 <98926546+CC0369@users.noreply.github.com>
2022-12-13 19:39:14 +0900

    placed player

--- python.py
+++ python.py
@@ -8,12 +8,14 @@ pygame.display.set_caption('Runner')
 clock = pygame.time.Clock()
 test_font = pygame.font.Font('src/assets/font/font.ttf', 30)
 
-sky_surface = pygame.image.load('src/assets/img/background.png')
-ground_surface = pygame.image.load('src/assets/img/ground.png')
+sky_surface = pygame.image.load('src/assets/img/background.png').convert()
+ground_surface = pygame.image.load('src/assets/img/ground.png').convert_alpha()
 text_surface = test_font.render('My game', False, 'Black')
 
-slime_surface = pygame.image.load('src/assets/img/slime.png')
+slime_surface = pygame.image.load('src/assets/img/slime.png').convert_alpha()
 slime_x_pos = 600
+
+player_surf = pygame.image.load('src/assets/img/character2.png').convert_alpha()
 
 while True:
     for event in pygame.event.get():
@@ -29,6 +31,8 @@ while True:
     slime_x_pos -= 3
     if slime_x_pos < -100:
         slime_x_pos = 800
+        
+    screen.blit(player_surf, (89, 200))
             
     pygame.display.update()
     clock.tick(60)
